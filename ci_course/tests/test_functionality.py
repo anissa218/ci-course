0a918295d456337904bd175058ff49fbda705177
anissa218 <73247232+anissa218@users.noreply.github.com>
2022-10-17 17:36:04 +0100

    update edge case test

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -13,7 +13,7 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1, 2, 3), 1)
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
-        self.assertEqual(ci_course.minimum('string',2),None)
+        self.assertEqual(ci_course.minimum('string'),None)
 
 
 if __name__ == '__main__':
